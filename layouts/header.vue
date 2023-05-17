e215b7cfaefe5a1a00c69967f3a04ca9032b3c2c
Rivo Yajnatantra Izzulhaq <rivoyajnatantraizzulhaq@Rivos-MacBook-Air.local>
2023-05-17 15:29:00 +0700

    Terbaru

--- layouts/header.vue
+++ layouts/header.vue
@@ -5,14 +5,14 @@
           <div class="flex justify-between">
             <div class="flex space-x-4">
               <div>
-                <a href="#" class="flex item-center py-2 px-2 text-gray-500">
+                <NuxtLink to="/" class="flex item-center py-2 px-2 text-gray-500">
                   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 mr-1 text-blue-500">
                   <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
                   </svg>
                   <span class="font-bold">
                     HOME PAGES
                   </span>
-                </a>
+                </NuxtLink>
               </div>
               <div class="flex item-center space-x-3 py-2 px-3 text-gray-500">
                 <NuxtLink to="/">About us</NuxtLink>
@@ -26,6 +26,9 @@
         </div>
       </div>
     </nav>
+    <div>
+      <h1 class="text-center bg-yellow-400 hover:bg-yellow-600 text-4xl">Welcome To Cooking Blogs</h1>
+    </div>
   </header>
   <slot />
 </template>
--- pages/blog/[id].vue
+++ pages/blog/[id].vue
@@ -16,10 +16,6 @@
         <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Est expedita illum voluptatum esse ipsa modi veritatis, nam molestias omnis quis nemo blanditiis suscipit illo ipsum, dolorum quidem? Commodi, voluptate porro! Voluptates iusto doloribus ut molestias. Quos, corrupti aliquid quas obcaecati dolore eveniet nobis repellat ea quisquam quae repellendus consequuntur libero.</p>
     </div>
 
-    <div class="flex item-center space-x-2 -top-5">
-        <NuxtLink class="py-3 px-3 bg-red-400 text-white-900 rounded hover:bg-red-300 hover:text-white-800" to="/">Home</NuxtLink>
-    </div>
-
 </template>
 
 <script setup>
